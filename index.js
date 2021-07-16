f2e5b067b1628845791fb418d6d80b29a6426614
eric <swimleft@gmail.com>
2021-07-15 20:21:16 -0700

    auth mostly done

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const keys=require('./config/keys')
+const cookieSession =require('cookie-session')
+const passport = require('passport')
+
+require('./models/user')
+//this just runs the passport.js file
+
+require('./services/passport');
+
+//
+
+
+
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }).then(console.log('connected')).catch((err)=> {
+      console.log(err)
+  });
+
 const app = express();
 
+app.use(
+    cookieSession({
+        maxAge: 30 *24 *60*60*1000,
+        keys:[keys.cookieKey]
+    })
+);
 
-app.get("/",(req,res) => {
-    res.send({
-        hi:'there shnewpy',
-        thegal: "is not moopy I hope"
-})
-})
+app.use(passport.initialize());
+app.use(passport.session())
+
+require('./routes/authRoutes')(app);
+
+
+
 
 const PORT = process.env.PORT || 5000;
 
