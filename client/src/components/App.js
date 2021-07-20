b2f957de21f42a53cdf2f371059ecdf95250851c
Eric Harvey <swimleft@gmail.com>
2021-07-20 16:50:39 -0700

    moving into front end

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App =() => {
+    return (
+        <div>
+            Hi There!
+            <p> WTF is going on with REACT????</p>
+            
+        </div>
+
+    )
+    
+}
+
+export default App;
